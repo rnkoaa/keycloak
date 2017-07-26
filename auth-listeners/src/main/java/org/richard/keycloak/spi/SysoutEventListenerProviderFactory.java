724a7e0d8b0e2fd7357a7dcadae89cdcc88b50d0
rnkoaa <richard.agyei@gmail.com>
2017-07-26 09:43:50 -0500

    Add authentication listener module

---
+++ auth-listeners/src/main/java/org/richard/keycloak/spi/SysoutEventListenerProviderFactory.java
@@ -0,0 +1,62 @@
+package org.richard.keycloak.spi;
+
+import org.keycloak.Config;
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by 7/26/17.
+ */
+public class SysoutEventListenerProviderFactory implements EventListenerProviderFactory {
+
+    private Set<EventType> excludedEvents;
+    private Set<OperationType> excludedAdminOperations;
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new SysoutEventListenerProvider(excludedEvents, excludedAdminOperations);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+        String[] excludes = config.getArray("excludes");
+        if (excludes != null) {
+            excludedEvents = new HashSet<>();
+            for (String e : excludes) {
+                excludedEvents.add(EventType.valueOf(e));
+            }
+        }
+
+        String[] excludesOperations = config.getArray("excludesOperations");
+        if (excludesOperations != null) {
+            excludedAdminOperations = new HashSet<>();
+            for (String e : excludesOperations) {
+                excludedAdminOperations.add(OperationType.valueOf(e));
+            }
+        }
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "sysout";
+    }
+}
